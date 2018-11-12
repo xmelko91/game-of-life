3a88379739d0738b4751ebbe4f9c5e43e199418d
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-11-12 18:31:10 +0300

    Set up project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Game of Life
+
+Project: Game of Life
+
+Go to the *src* directory, change *Main.java* and put your *.java* file(s) in it.
---
+++ src/gameoflife/Main.java
@@ -0,0 +1,7 @@
+package gameoflife;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
